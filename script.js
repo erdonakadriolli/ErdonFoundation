bdca8b3a6c97d3468de495bc399c4a20e6c2046a
unknown <donakadriolli@gmail.com>
2021-12-02 12:42:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let username = 'Erdona Kadriolli' ;
+let birth_year = 2005
+let isKid= false;
+let isAdulit = true;
+let height = 1.63;
+let temperature = 14;
+let quotes = 'A good Life';
+
+let user_email;
+let score = null;
+
+
+console.log('Hello world! My first code js');
+console.log('quotes');
+console.log('Life is good')
